1c936599473125ac8591e73b91afc9eacd602f55
KZTN <kztndev@gmail.com>
2019-10-25 10:05:57 -0300

    added some meta tags

--- calcularIMC.php
+++ calcularIMC.php
@@ -23,22 +23,18 @@ $sqlconnection->InsertTable($data->getGuests(), $data->getSum_imc());
     <head>
         <!-- Primary Meta Tags -->
         <title>Calculadora de Romão</title>
-        <meta name="title" content="Calculadora de Romão">
-        <meta name="description" content="A balança já não te dá o peso, te dá os pêsames...">
-        <!-- Open Graph / Facebook -->
+        <meta property="og:title" content="Calculadora de Romão">
         <meta property="og:type" content="website">
         <meta property="og:url" content="https://calculadoraimc.herokuapp.com/">
-        <meta property="og:title" content="Calculadora de Romão">
         <meta property="og:description" content="A balança já não te dá o peso, te dá os pêsames...">
         <meta property="og:image" content="https://calculadoraimc.herokuapp.com/src/img/romao.jpg">
-        <!-- Twitter -->
-        <meta property="twitter:card" content="summary_large_image">
-        <meta property="twitter:url" content="https://calculadoraimc.herokuapp.com/">
-        <meta property="twitter:title" content="Calculadora de Romão">
-        <meta property="twitter:description" content="A balança já não te dá o peso, te dá os pêsames...">
-        <meta property="twitter:image" content="https://calculadoraimc.herokuapp.com/src/img/romao.jpg">
-        <link href="../lib/bootstrap.min.css" type="text/css" rel="stylesheet" />
+        <meta property="og:image:width" content="600" />
+        <meta property="og:image:height" content="600" />
+        <div id='hidden' style='display:none;'><img src="https://calculadoraimc.herokuapp.com/src/img/romao.jpg"></div>
 
+
+
+        <link href="../lib/bootstrap.min.css" type="text/css" rel="stylesheet" />
     </head>
     <body>
         <!-- As a link -->
